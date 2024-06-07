5b1cfbfd13241de31c9bd67adef52f228c2a2fe8
Sandra Prestel <38217124+SandraPrestel@users.noreply.github.com>
2024-06-07 13:03:17 +0200

    Test CV2

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
 import sys
 import os
+import inspect
 import time
+import cv2
 from picamera2 import Picamera2
 
 def to_node(type, message):
@@ -14,21 +16,40 @@ def to_node(type, message):
     sys.stdout.flush()
 
 
-to_node("status", "Emotion Recognition started...")
+to_node("status", "Backend loaded...")
 
 # variables
 detected_emotion = "none"
+path_to_file = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+
+# setup face detector
+face_detector = cv2.CascadeClassifier(path_to_file + "/face_detection/haarcascade_frontalface_default.xml")
+cv2.startWindowThread()
+to_node("status", "CV2 started...")
 
 # start the camera
 picam2 = Picamera2()
-camera_config = picam2.create_preview_configuration()
+camera_config = picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)})
 picam2.configure(camera_config)
 picam2.start()
 
 time.sleep(2)
 picam2.capture_file("testPython.jpg")
+to_node("status", "Testimage saved...")
+
+# capture frame
+img = picam2.capture_array()
+
+# detect faces in frame
+grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+faces = face_detector.detectMultiScale(grey, 1.1, 5)
+
+# debugging
+noFaces = len(faces)
+returnMessage = "Detected " + noFaces + " faces"
+
 
 picam2.stop()
 
 # dummy result
-to_node('result', {'emotion': 'File Saved'})
+to_node('result', {'emotion': returnMessage})    #TODO: Replace by detected emotion
